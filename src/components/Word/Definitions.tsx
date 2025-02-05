24dfeb7e73b3850614fad442fe785a574e63c210
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2025-02-05 20:11:49 +0200

    refactor: Definitions component - Added input field toggle for definitions - Used `useState` to manage the input field visibility - Updated `updateWord` to add new definitions dynamically - Rendered definitions inside `InfoBox`

---
+++ src/components/Word/Definitions.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Operation } from "../../styledComponents/Operation";
+import { Icon } from "../../ui/Icon";
+import { RiAddLine } from "react-icons/ri";
+import { Input } from "../../styledComponents/Input";
+import { Text } from "../../ui/Text";
+import { useWord } from "../../context/WordContext";
+import { InfoBox  ,Info} from "../../styledComponents/InfoBox";
+
+function Definitions() {
+  const [isAdd, setIsAdd] = useState(false);
+  const {updateWord ,word}= useWord();
+
+  return (
+    <>
+    <Operation> 
+      <Text>Definitions</Text>
+      <Icon as={RiAddLine} onClick={() => setIsAdd(prev => !prev)} />
+      {isAdd && (
+        <Input 
+          placeholder="Write a definitions here.."
+          action={(e)=>updateWord({definitions: [e.target.value.trim()]})}
+        />
+      )}
+    </Operation>
+          <InfoBox>
+          {
+            word.definitions.map((definition ,index)=> <Info key={index}>{definition}</Info>)
+          }
+          </InfoBox>
+    </>
+  );
+}
+
+export default Definitions;
