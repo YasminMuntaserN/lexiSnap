212268cb0a2d440ccdeb1d31d8b95201644b78cf
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2025-02-06 20:43:09 +0200

    feat: add translation component to add translation for the word

---
+++ src/components/Word/Translation.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Operation } from "../../styledComponents/Operation";
+import { Icon } from "../../ui/Icon";
+import { RiAddLine } from "react-icons/ri";
+import { Input } from "../../styledComponents/Input";
+import { Text } from "../../ui/Text";
+import { useWord } from "../../context/WordContext";
+import { Info, InfoBox } from "../../styledComponents/InfoBox";
+
+function Translation() {
+  const [isAdd, setIsAdd] = useState(false);
+  
+  const {updateWord ,word}= useWord();
+  return (
+    <>
+    <Operation> 
+      <Text>Add a translation</Text>
+      <Icon as={RiAddLine} onClick={() => setIsAdd(prev => !prev)} />
+      {isAdd && (
+      <Input 
+        placeholder="Add Translation..." 
+        action={(e)=>updateWord({translations: [e.target.value.trim()]})}
+      />
+      )}
+    </Operation>
+      <InfoBox>
+      {
+        word.translations.map((translation ,index)=> <Info key={index}>{translation}</Info>)
+      }
+      </InfoBox>
+    </>
+  );
+}
+
+export default Translation;
