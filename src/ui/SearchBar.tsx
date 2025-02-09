c4f520d944ae87d448ebe8c97d6a17559d270b26
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2025-02-09 18:23:37 +0200

    refactor: edit the searchBar component and make it reusable to search for tags and words

--- src/ui/SearchBar.tsx
+++ src/ui/SearchBar.tsx
@@ -1,6 +1,9 @@
 import { FaSearch } from "react-icons/fa";
 import styled from "styled-components";
 import { media } from "../styledComponents/Media";
+import { useSearch } from "../components/Word/hooks/useSearch";
+import { useWord } from "../context/WordContext";
+import { useEffect, useState } from "react";
 
 const Input = styled.input`
   border-radius: 1.5rem; 
@@ -13,18 +16,54 @@ const Input = styled.input`
       width: 200px; 
     `}
 `;
+interface SearchBarProps{
+  placeholder:string;
+  style?:object;
+  type?:string;
+}
+function SearchBar({ placeholder, style, type }: SearchBarProps) {
+  const [inputValue, setInputValue] = useState("");
+  const { mutate, isLoading, error } = useSearch();
+  const { WordsPage, TagsPage, setSearchInfo } = useWord();
 
-function SearchBar(){
+  const handleSearch = () => {
+    setSearchInfo({ list:[], isLoading:false, error:null ,isEmpty:true ,isSearch:false});
+
+    if (inputValue.length < 2) return; 
+    mutate({ query: inputValue, page: type === "tags" ? TagsPage : WordsPage }, {
+      onSuccess: (data) => {
+        setSearchInfo({ 
+          list: data || [], 
+          isLoading, 
+          error,
+          isEmpty: !data || data.length === 0 
+          ,isSearch:true
+        });
+      }
+    });
+  };
+  useEffect(()=> { 
+    if(inputValue.length === 0) setSearchInfo({ list:[], isLoading:false, error:null ,isEmpty:true ,isSearch:false })
+    else 
+    setSearchInfo({isSearch:true })
+  },[inputValue ,setSearchInfo] )
   return (
-    <div style={{position:"relative" }}>
+    <div style={{ position: "relative" }}>
       <Input
-        placeholder="Search for a Word..."
-        // onChange={handleInputChange}
+        placeholder={placeholder}
+        value={inputValue}
+        onChange={(e) =>setInputValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && !e.shiftKey) {
+            e.preventDefault();
+            handleSearch();
+          }
+        }}
         type="text"
+        style={style}
       />
-      <FaSearch style={{position:"absolute",right:'20px' ,top:'5px',fontSize:'27px' }}/>
+      <FaSearch style={{ position: "absolute", right: `${style ? "50px" : "20px"}`, top: "5px", fontSize: "27px" }} />
     </div>
   );
 }
-
 export default SearchBar;
